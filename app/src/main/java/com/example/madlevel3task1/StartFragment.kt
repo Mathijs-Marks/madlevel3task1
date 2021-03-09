3d62f137f44a00cf57c68096786f48291220530f
DESKTOP-NLFJPG5\mathi <mathijsmarks@msn.com>
2021-03-09 23:25:21 +0100

    Refactored the basic activity template, implemented StartFragment.

--- app/src/main/java/com/example/madlevel3task1/StartFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/StartFragment.kt
@@ -5,27 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.madlevel3task1.databinding.FragmentStartBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class StartFragment : Fragment() {
+
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+
+        binding.btnStartRating.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_startFragment_to_ratingFragment
+            )
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
